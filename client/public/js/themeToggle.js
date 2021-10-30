237ee27eb3286e739fd71081bcb118b960d87dd2
NerdyVisky <vishvesh106@gmail.com>
2021-10-31 01:19:09 +0530

    cleanup

--- client/public/js/themeToggle.js
+++
@@ -1,4 +0,0 @@
-let toggleBtn = document.querySelector('.theme-toggle-button');
-        toggleBtn.addEventListener('click', () => {
-            document.body.classList.toggle('dark');
-        })
--- client/public/js/user.js
+++
@@ -1,9 +0,0 @@
-
-
-const setUserName = (e) => {
-e.preventDefault();
-const userName = document.querySelector('#avatarName').value;
-sessionStorage.setItem('userName', userName);
-console.log(sessionStorage.getItem('userName'));
-
-}
